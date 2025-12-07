eef802d39c4567e696d64ef590b8f59e52908075
Algorithmias <160455039+algorithmias@users.noreply.github.com>
2025-12-07 17:38:18 +1000

    delete irrelevant trendlines;

--- @scripts/generate_index.js
+++ @scripts/generate_index.js
@@ -1,37 +1,37 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 function directoryStructure(dir) {
-    let structure = {};
-    fs.readdirSync(dir).forEach(entry => {
-        let fullPath = path.join(dir, entry);
-        if (fs.lstatSync(fullPath).isDirectory()) {
-            structure[entry] = directoryStructure(fullPath);
-        } else {
-            structure[entry] = entry;
-        }
-    });
-    return structure;
+  let structure = {};
+  fs.readdirSync(dir).forEach(entry => {
+    let fullPath = path.join(dir, entry);
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      structure[entry] = directoryStructure(fullPath);
+    } else {
+      structure[entry] = entry;
+    }
+  });
+  return structure;
 }
 
 function makeDirectory(buildPath) {
-    if (!fs.existsSync(buildPath)) {
-        fs.mkdirSync(buildPath);
-    }
+  if (!fs.existsSync(buildPath)) {
+    fs.mkdirSync(buildPath);
+  }
 
 }
 
-const dataPaths = ["moments", "signals", "trendlines"];
+const dataPaths = ['moments', 'signals', 'trendlines'];
 
 for (const dataPath of dataPaths) {
-    const directory = path.join(process.cwd(), dataPath);
-    const structure = directoryStructure(directory);
+  const directory = path.join(process.cwd(), dataPath);
+  const structure = directoryStructure(directory);
 
-    const rootDirectory = path.join(process.cwd());
-    makeDirectory(rootDirectory);
-    fs.writeFileSync(path.join(rootDirectory, `${dataPath}.json`), JSON.stringify(structure, null, 4));
-    
-    const buildDirectory = path.join(process.cwd(), "_data");
-    makeDirectory(buildDirectory);
-    fs.writeFileSync(path.join(buildDirectory, `${dataPath}.json`), JSON.stringify(structure, null, 4));
+  const rootDirectory = path.join(process.cwd());
+  makeDirectory(rootDirectory);
+  fs.writeFileSync(path.join(rootDirectory, `${dataPath}.json`), JSON.stringify(structure, null, 4));
+
+  const buildDirectory = path.join(process.cwd(), '_data');
+  makeDirectory(buildDirectory);
+  fs.writeFileSync(path.join(buildDirectory, `${dataPath}.json`), JSON.stringify(structure, null, 4));
 }
